10afa7987382007899aab1c229c9b71b2b34488b
hosituka <91714805+hosituka@users.noreply.github.com>
2025-09-30 00:34:16 +0900

    Delete ActivityInfo/SoftCarousel.js

--- ActivityInfo/SoftCarousel.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const SoftCarouselContainer = document.querySelector(".SoftCaroselContaine");
-    const SoftCarousel = document.querySelector(".SoftCarousel");
-    const SoftCard = document.querySelectorAll(".SoftCard");
-    if (!SoftCarouselContainer) return;
-});
