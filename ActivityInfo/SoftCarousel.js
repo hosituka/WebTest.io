97d254f93e7bc6a4c8d3bf94fe28e80e9374c864
hosituka <91714805+hosituka@users.noreply.github.com>
2025-09-30 00:37:42 +0900

    Add files via upload

---
+++ ActivityInfo/SoftCarousel.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const SoftCarouselContainer = document.querySelector(".SoftCaroselContaine");
+    const SoftCarousel = document.querySelector(".SoftCarousel");
+    const SoftCard = document.querySelectorAll(".SoftCard");
+    if (!SoftCarouselContainer) return;
+});
